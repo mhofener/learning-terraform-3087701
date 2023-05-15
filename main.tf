5d855ac9e39160708b1704605e2e7b9aa5fcc166
mhofener <133658107+mhofener@users.noreply.github.com>
2023-05-15 19:52:24 +0100

    Add missing code

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -32,9 +32,9 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -46,9 +46,8 @@ module "blog_sg" {
   source              = "terraform-aws-modules/security-group/aws"
   version             = "4.17.2"
 
+  vpc_id              = module.blog_vpc.vpc_id
   name                = "blog"
-  vpc_id              = module.vpc.public_subnets[0]
-
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
